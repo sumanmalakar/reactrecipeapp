10424b174f56963de01d1db5e4e9b25f2dc136c5
Suman Malakar <malakars041@gmail.com>
2020-06-28 22:21:32 +0530

    changing index.js deleting serviceworker.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
 
 
 
@@ -16,4 +16,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorker.unregister();
+
